fb67f365f0dcad2c9f11eb4491fed1966056874b
Hritik Adhikari <hritikadhikari05@gmail.com>
2022-09-25 13:49:16 +0530

    initial commit

--- router/adminRoutes.js
+++ router/adminRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth")
 
 const adminController = require("../controllers/adminController");
 
-router.get("/status/:status", adminController.getPostsByStatus);
-router.post("/update-status", adminController.updatePostStatus);
-router.post("/assign-reward-point", adminController.assignRewardPoints);
+router.get("/status/:status",auth, adminController.getPostsByStatus);
+router.post("/update-status", auth,adminController.updatePostStatus);
+router.post("/assign-reward-point",auth, adminController.assignRewardPoints);
 
 module.exports = router;
--- router/dashboardRoutes.js
+++ router/dashboardRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth")
 
 const dashboardController = require("../controllers/dashboardController")
 
-router.get("/get-dashboard-details",dashboardController.getDashboardData);
+router.get("/get-dashboard-details",auth,dashboardController.getDashboardData);
 
 
 
